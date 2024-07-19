cf70fc42a5000afb684d3c5c3631a42c28638ec7
Nishant Kumar Gupta <nishantkumargupta728@gmail.com>
2024-07-19 15:07:58 +0530

    new commit for portfolio

---
+++ script.js
@@ -0,0 +1,7 @@
+const typed = new Typed('.multiple-text' , {
+    strings: ['Full stack developer' , 'Web Designer' , 'Youtuber', 'Coder'],
+    typeSpeed: 70,
+    backSpeed: 70,
+    backDelay:1000,
+    loop:true
+})
